39060a9c51a0f5d994379c7a311205fcc417ecaa
YanaKorsak2310 <kazantip2310@gmail.com>
2024-04-05 13:55:54 +0200

    ejercicio03

--- ejercicio03/ejercicio03.js
+++ ejercicio03/ejercicio03.js
@@ -5,8 +5,16 @@
  * Recuerda usar la estructura condicional If.
  */
 
-function compareThreeNumbers() {
-//Escribe tu codigo aqui
+function compareThreeNumbers(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    return num1
+  } else if (num2 > num3 && num2 != num1) {
+    return num2
+  } else if (num3 != num1) {
+    return num3
+  } else {
+    return 'iguales'
+  }
 }
-
-module.exports = { compareThreeNumbers };
+console.log(compareThreeNumbers(5, 8, 2))
+module.exports = { compareThreeNumbers }
